ec74f4b7daea8ed6ccb4adf408f37dfc6e8fe317
Taisuke Fukuno <fukuno@jig.jp>
2023-03-22 22:42:00 +0900

    add debug on handleWeb

--- aichat.js
+++ aichat.js
@@ -1,4 +1,5 @@
-import { serveAPI } from "https://js.sabae.cc/wsutil.js";
+//import { serveAPI } from "https://js.sabae.cc/wsutil.js";
+import { serveAPI } from "./wsutil.js";
 import { fetchChat } from "./fetchChat.js";
 
 serveAPI("/api", async (param) => {
