6dd561846d18f5c9e1afa9ed44203a104a067305
Taisuke Fukuno <fukuno@jig.jp>
2023-07-07 10:02:55 +0900

    use gpt-4

--- fetchConversation.js
+++ fetchConversation.js
@@ -28,9 +28,9 @@ export const fetchConversation = async (messages, funcs) => {
       const len = new TextEncoder().encode(JSON.stringify(messages) + JSON.stringify(functions)).length;
       //console.log(len);
       //req.model = len > 3000 ? "gpt-3.5-turbo-16k-0613" : "gpt-3.5-turbo-0613"; // gpt-3.5-turbo-16k or gpt-3.5-turbo and gpt-3.5-turbo-0301 are supported.
-      req.model = "gpt-4-0613"; // ok
+      //req.model = "gpt-4-0613"; // ok
       //req.model = "gpt-4-0314"; // ChatGPT4を認識していない？
-      //req.model = "gpt-4"; // 料金30倍 API使用ok
+      req.model = "gpt-4"; // 料金30倍 API使用ok
       const res = await fetchCompletions(req);
       if (res.error) {
         return "error: " + res.error.message;
