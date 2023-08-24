7ff928a51b6c72454c8c4e539dcf5ccc72f76423
Taisuke Fukuno <fukuno@jig.jp>
2023-08-24 21:05:24 +0900

    fix ichigo-ai

--- aichat.js
+++ aichat.js
@@ -2,7 +2,7 @@ import { serveAPI } from "https://js.sabae.cc/wsutil.js";
 import { fetchChat } from "./fetchChat.js";
 import { fetchConversation } from "./fetchConversation.js";
 import { fetchConversationWithKanko } from "./fetchConversationWithKanko.js";
-import { fetchConversationIchigo } from "./fetchConversationIchigo.js";
+import { fetchConversationByModel } from "./fetchConversationByModel.js";
 
 serveAPI("/api", async (param, req, path) => {
   if (path == "/api/conversation") {
@@ -10,7 +10,7 @@ serveAPI("/api", async (param, req, path) => {
   } else if (path == "/api/conversation-kanko") {
     return await fetchConversationWithKanko(param);
   } else if (path == "/api/conversation-ichigo") {
-    return await fetchConversationWithModel("ft:gpt-3.5-turbo-0613:jig-jp::7r27I7v8", param);
+    return await fetchConversationByModel("ft:gpt-3.5-turbo-0613:jig-jp::7r27I7v8", param);
   }
   return await fetchChat(param);
 });
