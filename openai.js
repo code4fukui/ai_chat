ba332d8d6e9d33f7f7de64cb4a6781c657d9900a
Taisuke Fukuno <fukuno@jig.jp>
2023-09-21 14:23:41 +0900

    export fetchAPI

--- openai.js
+++ openai.js
@@ -4,7 +4,7 @@ const KEY = await getEnv("OPENAI_API_KEY");
 
 // https://platform.openai.com/docs/api-reference/chat/create
 
-const fetchAPI = async (url, req) => {
+export const fetchAPI = async (url, req) => {
   const opt = {
     method: req ? "POST" : "GET",
     mode: "cors",
