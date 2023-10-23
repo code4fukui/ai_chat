09e9e89d9b1be498b74b78cbb7fc9551e6794b92
Taisuke Fukuno <fukuno@jig.jp>
2023-10-24 07:58:19 +0900

    add models page

--- aichat.js
+++ aichat.js
@@ -3,6 +3,7 @@ import { fetchChat } from "./fetchChat.js";
 import { fetchConversation } from "./fetchConversation.js";
 import { fetchConversationWithKanko } from "./fetchConversationWithKanko.js";
 import { fetchConversationByModel } from "./fetchConversationByModel.js";
+import { getModels } from "./openai.js";
 
 serveAPI("/api", async (param, req, path) => {
   if (path == "/api/conversation") {
@@ -11,6 +12,8 @@ serveAPI("/api", async (param, req, path) => {
     return await fetchConversationWithKanko(param);
   } else if (path == "/api/conversation-ichigo") {
     return await fetchConversationByModel("ft:gpt-3.5-turbo-0613:jig-jp::7r27I7v8", param);
+  } else if (path == "/api/models") {
+    return await getModels();
   }
   return await fetchChat(param);
 });
--- getModels.test.js
+++ getModels.test.js
@@ -2,6 +2,3 @@ import { getModels } from "./openai.js";
 
 const models = await getModels();
 console.log(models);
-
-const data = models.data.sort((a, b) => b.created - a.created);
-console.log(data.map(m => m.id + "," + new Date(m.created * 1000)));
