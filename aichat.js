a1f91760423aa02f43f26afd5d033fc484374bf5
Taisuke Fukuno <fukuno@jig.jp>
2024-06-06 19:06:07 +0900

    add image-recog

--- aichat.js
+++ aichat.js
@@ -4,6 +4,7 @@ import { fetchConversation } from "./fetchConversation.js";
 import { fetchConversationWithKanko } from "./fetchConversationWithKanko.js";
 import { fetchConversationByModel } from "./fetchConversationByModel.js";
 import { getModels } from "./openai.js";
+import { fetchImageRecog } from "https://code4fukui.github.io/openai-imagerecog/fetchImageRecog.js"
 
 serveAPI("/api", async (param, req, path) => {
   if (path == "/api/conversation") {
@@ -14,6 +15,8 @@ serveAPI("/api", async (param, req, path) => {
     return await fetchConversationByModel("ft:gpt-3.5-turbo-0613:jig-jp::7r27I7v8", param);
   } else if (path == "/api/models") {
     return await getModels();
+  } else if (path == "/api/imagerecog") {
+    return await fetchImageRecog(param.imgbin, param.prompt);
   }
   return await fetchChat(param);
 });
