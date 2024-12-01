6c76e89e09eaf3c9b2d56202e4ba624fcd404b8e
Taisuke Fukuno <fukuno@jig.jp>
2024-12-01 11:02:18 +0900

    update model

--- fetchChat.js
+++ fetchChat.js
@@ -3,14 +3,15 @@ import { DateTime } from "https://js.sabae.cc/DateTime.js";
 
 export const fetchChat = async (prompt) => {
   const req = {
-    model: "gpt-3.5-turbo", // gpt-3.5-turbo and gpt-3.5-turbo-0301 are supported.
+    //model: "gpt-3.5-turbo", // gpt-3.5-turbo and gpt-3.5-turbo-0301 are supported.
+    model: "gpt-4o-2024-11-20",
     messages: [
       { "role": "user", "content": prompt },
     ],
   };
-  console.log(prompt);
+  //console.log(prompt);
   const res = await fetchCompletions(req);
-  console.log(res);
+  //console.log(res);
   const answer = res.error ? "error: " + res.error.message : res.choices[0].message.content;
   //const answer = res.choices[0].message.content;
   const dt = new DateTime();
